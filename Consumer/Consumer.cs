3d17b770d1e034d362edb50d20abe5196f7a2e8e
Nelson Baggio <nelson.baggio@gmail.com>
2019-04-12 11:16:55 -0300

    removing environment variables from customer and no try catch

--- Consumer/Consumer.cs
+++ Consumer/Consumer.cs
@@ -12,44 +12,37 @@ namespace Consumer
     {
         static void Main(string[] args)
         {
-            try
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
             {
-                var factory = new ConnectionFactory() { HostName = "localhost" };
-                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
                 {
-                    using (var channel = connection.CreateModel())
+                    channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    var database = new Database().Connect();
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, input) =>
                     {
-                        channel.QueueDeclare(queue: "hello",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
+                        var body = input.Body;
+                        var message = Encoding.UTF8.GetString(body);
 
-                        var database = new Database().Connect();
-                        var consumer = new EventingBasicConsumer(channel);
-                        consumer.Received += (model, input) =>
-                        {
-                            var body = input.Body;
-                            var message = Encoding.UTF8.GetString(body);
+                        BsonDocument book = new BsonDocument()
+                            .Add("date", new DateTime())
+                            .Add("message", message);
 
-                            BsonDocument book = new BsonDocument()
-                                .Add("date", new DateTime())
-                                .Add("message", message);
+                        database.From("DataBaseMessages").Create("CollectionMessages", book);
 
-                            database.From("DataBaseMessages").Create("CollectionMessages", book);
+                        Console.WriteLine(" [x] Received: {0}", message);
+                        Thread.Sleep(2000);
+                    };
 
-                            Console.WriteLine(" [x] Received: {0}", message);
-                            Thread.Sleep(2000);
-                        };
-
-                        channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
-                        Console.ReadLine();
-                    }
+                    channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+                    Console.ReadLine();
                 }
-            }
-            catch (Exception erro)
-            {
-                Console.WriteLine(erro.Message);
             }
         }
     }
